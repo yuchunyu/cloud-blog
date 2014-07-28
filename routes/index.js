7b79ba5c8a7d81e5d7b32ea73557b9add45ba5b7
yuchunyu <yuchunyu@goyoo.com>
2014-07-28 22:55:13 +0800

    update view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' ,name :'world'});
+});
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Express' ,name :'world'});
+});
+router.get('/reg', function(req, res) {
+  res.render('reg', { title: 'Express' ,name :'world'});
+});
+router.get('/logout', function(req, res) {
+  res.render('index', { title: 'Express' ,name :'world'});
+});
+router.get('/blog', function(req, res) {
   res.render('blog', { title: 'Express' ,name :'world'});
 });
+router.get('/article', function(req, res) {
+  res.render('article', { title: 'Express' ,name :'world'});
+});
+router.get('/publish', function(req, res) {
+  res.render('publish', { title: 'Express' ,name :'world'});
+});
+
 
 module.exports = router;
