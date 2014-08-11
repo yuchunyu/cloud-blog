63ce179d7a06c1034acbbeebdef92ccba75377f6
yuchunyu <yuchunyu@goyoo.com>
2014-08-11 21:56:15 +0800

    update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session'),
+    bodyParser = require('body-parser');
 
 var routes = require('./lib/route');
 
@@ -18,6 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(cookieSession({secret: 'your secret here'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
