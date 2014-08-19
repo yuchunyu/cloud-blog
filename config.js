ec0b891bdd5fb2f43627e0c3f208b7167e4fdb56
yuchunyu <yuchunyu@goyoo.com>
2014-08-20 00:00:24 +0800

    update

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	MONGO_URI: "mongodb://127.0.0.1:27017/mygithub",
-	LISTEN_PORT: process.env.PORT || 9216
+	LISTEN_PORT: process.env.PORT || 9216,
+	INDEXPAGENUM: 3,//首页一页显示几个文章
+	INDEXWORDS:104//首页一个文章多少字
 }
